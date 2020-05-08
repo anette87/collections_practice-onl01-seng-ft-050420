5da8e7a182906f20fae1b1c3b885db920a166afb
Anette Rivera <anettem.rivera@gmail.com>
2020-05-08 03:03:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,14 +35,5 @@ def sum_array(array)
     sum+=num
   end
   sum
-end 
-
-def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
-    else
-      word + "s"
-    end
-  end
+end
 
