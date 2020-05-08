4a7956fbde0fbbe6a822e2e9b15833b213e57107
Anette Rivera <anettem.rivera@gmail.com>
2020-05-08 02:51:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(arr)
+    arr.sort
+end
 
+def sort_array_desc(arr)
+    arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |word1, word2| word1.length <=> word2.length }
+end  
+
+def swap_elements(arr) 
+   return arr << arr.delete_at(1)
+end 
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr) 
+   arr.each do |word|
+  return word[2].replace("$")
+   end
+end 
